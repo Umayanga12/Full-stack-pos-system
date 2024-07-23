c02091b70d4d41933a066334c5b3ee59fc98809f
Umayanga12 <umayangaathapaththu05@gmail.com>
2024-07-23 22:12:49 +0530

    change file structure

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -28,7 +28,7 @@ export function Login() {
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" placeholder="Password" />
+              <Input id="password" placeholder="Password" type="password"/>
             </div>
           </div>
         </form>
---
+++ src/components/brandDetail.tsx
@@ -0,0 +1,70 @@
+import { Button } from "@/components/ui/button"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  
+  const invoices = [
+    {
+      invoice: "INV001",
+      paymentStatus: "Paid",
+      totalAmount: "$250.00",
+
+    },
+    {
+      invoice: "INV002",
+      paymentStatus: "Pending",
+      totalAmount: "$150.00",
+
+    },
+    {
+      invoice: "INV003",
+      paymentStatus: "Unpaid",
+      totalAmount: "$350.00",
+  
+    },
+    {
+      invoice: "INV004",
+      paymentStatus: "Paid",
+      totalAmount: "$450.00",
+  
+    },
+    {
+      invoice: "INV005",
+      paymentStatus: "Paid",
+      totalAmount: "$550.00",
+     
+    },
+   
+  ]
+  export function BrandtList() {
+    return (
+      <Table>
+        <TableCaption>A list of excisting Brands</TableCaption>
+        <TableHeader className="bg-black">
+          <TableRow>
+            <TableHead className="w-[150px] text-white">Brand Name</TableHead>
+            <TableHead className="text-white">Email</TableHead>
+            <TableHead className="text-white">Contact</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+              <TableCell>{invoice.paymentStatus}</TableCell>
+              <TableCell className="text-right pr-5">{invoice.totalAmount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        
+      </Table>
+    )
+  }
+  
