8a37e3dbf151acf474b92cc6e84813c120d8e7b7
Umayanga12 <umayangaathapaththu05@gmail.com>
2024-08-08 13:17:47 +0530

    conntect tp api

--- src/routers/brandRouter.js
+++ src/routers/brandRouter.js
@@ -4,6 +4,7 @@ const brandController= require('../controllers/brandController');
 const brandrouter = expresa.Router();
 
 brandrouter.get('/brands', brandController.getBrands);
+brandrouter.get('/getbrand/:id',brandController.getBrandbyId);
 brandrouter.post('/createbrands', brandController.createBrand);
 brandrouter.put('/updatebrands/:id', brandController.updateBrand);
 brandrouter.delete('/deletebrands/:id', brandController.deleteBrand);
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -4,6 +4,7 @@ const Controllers= require('../controllers/productController');
 const productsrouter = express.Router();
 
 productsrouter.get('/products', Controllers.getProducts);
+productsrouter.get('/getproduct/:id',Controllers.getProductById);
 productsrouter.post('/addproducts', Controllers.createProduct);
 productsrouter.put('/products/:id', Controllers.updateProduct);
 productsrouter.delete('/deleteproducts/:id', Controllers.deleteProduct);
